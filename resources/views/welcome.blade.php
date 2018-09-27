2c29071e07f1816c0041165cd272e79515b67475
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2018-09-26 21:14:36 -0700

    Edit environments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Trojan-Panel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -78,16 +78,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Trojan-Panel
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://trojan-gfw.github.io/trojan/">Trojan</a>
+                    <a href="https://github.com/trojan-gfw/trojan-panel">GitHub</a>
                 </div>
             </div>
         </div>
