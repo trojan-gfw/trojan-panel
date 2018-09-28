4b7420b6fe06e8f7f569c8953241820bd72de775
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2018-09-28 10:53:21 -0700

    Fix indentation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,32 +34,32 @@
                 <div class="card-header">Account Details</div>
                 <div class="card-body">
                     <table class="table table-bordered">
-                      <tbody>
-                        <tr>
-                          <th scope="row">Username</th>
-                          <td>{{ Auth::user()->username }}</td>
-                        </tr>
-                        <tr>
-                          <th scope="row">Email</th>
-                          <td>{{ Auth::user()->email }}</td>
-                        </tr>
-                        <tr>
-                          <th scope="row">Quota</th>
-                          <td>{{ $quota }}</td>
-                        </tr>
-                        <tr>
-                          <th scope="row">Download</th>
-                          <td>{{ $download }}</td>
-                        </tr>
-                        <tr>
-                          <th scope="row">Upload</th>
-                          <td>{{ $upload }}</td>
-                        </tr>
-                        <tr>
-                          <th scope="row">Remain</th>
-                          <td>{{ $remain }}</td>
-                        </tr>
-                      </tbody>
+                        <tbody>
+                            <tr>
+                                <th scope="row">Username</th>
+                                <td>{{ Auth::user()->username }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Email</th>
+                                <td>{{ Auth::user()->email }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Quota</th>
+                                <td>{{ $quota }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Download</th>
+                                <td>{{ $download }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Upload</th>
+                                <td>{{ $upload }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Remain</th>
+                                <td>{{ $remain }}</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
