3893673fde64d1f9f8cc7640cd3d4f962ae9a758
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2018-09-27 21:39:02 -0700

    Add UserController

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::user() && Auth::user()->is_admin == 1) {
+            return $next($request);
+        }
+        return redirect('/home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('users', 'UserController');
