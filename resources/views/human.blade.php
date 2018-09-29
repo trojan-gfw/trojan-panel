e5e70bebdfedb8105eadd62dc4ccdb129206d0b5
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2018-09-29 12:29:16 -0700

    unsigned int

--- resources/views/human.blade.php
+++ resources/views/human.blade.php
@@ -3,7 +3,7 @@
     {
         $size = array('B','KB','MB','GB','TB','PB','EB','ZB','YB');
         $factor = floor((strlen($bytes) - 1) / 3);
-        return sprintf("%.{$decimals}f %s (%d Bytes)", $bytes / pow(1024, $factor), @$size[$factor], $bytes);
+        return sprintf("%.{$decimals}f %s (%u Bytes)", $bytes / pow(1024, $factor), @$size[$factor], $bytes);
     }
     function human_string($quota, $download, $upload)
     {
