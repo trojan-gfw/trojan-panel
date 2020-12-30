c0d9b7a2127760b1e1baf5ac4df362065976dda8
MargaretteMoss <53471588+MargaretteMoss@users.noreply.github.com>
2020-12-30 23:18:23 +0800

    This commit optimizes the user list that has insufficient display width.
    Signed-off-by: MargaretteMoss <53471588+MargaretteMoss@users.noreply.github.com>

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,7 +6,7 @@
 
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">User List</div>
                 <div class="card-body">
